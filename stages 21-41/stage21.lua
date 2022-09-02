fb4ae570b9128dd1fe9c7b801d643712342da4cd
Sathvik <88944590+User319183@users.noreply.github.com>
2022-09-02 16:33:25 -0400

    Add files via upload
    Working on script 31+

---
+++ stages 21-41/stage21.lua
@@ -0,0 +1,20 @@
+local player = game.Players.LocalPlayer
+
+
+
+for i = 1, 1 do
+wait(1)
+player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame + Vector3.new(-85,0,0)
+wait(1)
+player.Character.Humanoid.Jump = true
+wait(1)
+player.Character.Humanoid.Health = 0
+wait(1)
+
+
+
+--end of stage 21--
+
+
+
+end
---
+++ stages 21-41/stage22.lua
@@ -0,0 +1,17 @@
+local player = game.Players.LocalPlayer
+
+
+
+for i = 1, 1 do
+wait(1)
+player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame + Vector3.new(0,0,-120)
+wait(1)
+player.Character.Humanoid.Jump = true
+wait(1)
+player.Character.Humanoid.Health = 0
+wait(1)
+--end of stage 22--
+
+
+
+end
